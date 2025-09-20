66c5fd0a1026ef5c7d7d00d5928e3c7e8d0e7bde
Siddha <siddharoodha1993@gmail.com>
2025-09-20 19:47:48 +0530

    20 Sep 2025

---
+++ src/ex_04_Operators/Lab053_Interview.java
@@ -0,0 +1,15 @@
+package ex_04_Operators;
+
+public class Lab053_Interview {
+
+    public static void main(String[] args) {
+
+        long l = 100l; // Byte, Bits ->
+
+        String name = "Pramod"; // Byte, Bits ->
+
+        // Byte, Bits -> 8 Bytes, 64 Bits
+
+
+    }
+}
---
+++ src/ex_04_Operators/Lab055_Interview.java
@@ -0,0 +1,11 @@
+package ex_04_Operators;
+
+public class Lab055_Interview {
+
+    public static void main(String[] args) {
+
+        System.out.println('A' == 65);
+
+
+    }
+}
