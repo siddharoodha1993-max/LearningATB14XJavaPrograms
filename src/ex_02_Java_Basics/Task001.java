c01ce1fec00123f850541a32e130d97c593695a3
Siddha <siddharoodha1993@gmail.com>
2025-09-20 20:22:31 +0530

    20 Sep 2025

---
+++ src/ex_02_Java_Basics/Task001.java
@@ -0,0 +1,32 @@
+package ex_02_Java_Basics;
+
+public class Task001 {
+
+    public static void main(String[] args) {
+
+        // Print the table of 9 or 5 using the System.out.printf
+      //  int num = 9;
+        // 9x1=9
+        //int -> %d
+        /*System.out.printf("%d x 1 = %d", num, num*1).println();
+        System.out.printf("%d x 2 = %d", num, num*2).println();
+        System.out.printf("%d x 3 = %d", num, num*3).println();
+        System.out.printf("%d x 4 = %d", num, num*4).println();
+        System.out.printf("%d x 5 = %d", num, num*5).println();
+        System.out.printf("%d x 6 = %d", num, num*6).println();
+        System.out.printf("%d x 7 = %d", num, num*7).println();
+        System.out.printf("%d x 8 = %d", num, num*8).println();
+        System.out.printf("%d x 9 = %d", num, num*9).println();
+        System.out.printf("%d x 10 = %d", num, num*10).println();
+*/
+        int num = 5;
+        // 5 x 1 = 5
+        // int -> %d
+        System.out.printf("%d x 1 = %d", num, num*1).println();
+        System.out.printf("%d x 2 = %d", num, num*2).println();
+        System.out.printf("%d x 3 = %d", num, num*3).println();
+
+
+
+    }
+}
